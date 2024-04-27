fc4ec7a7cb759370fbc1f94d5c48a0a51dc4107b
ashxsong <beethoven6198@gmail.com>
2024-04-26 19:13:34 -0500

    Add drawing feature
    Enabled color of grid divs to change as mouse hovers over them

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ for (let i = 0; i < 16 * 16; i++) {
   square.classList.add("square");
   container.appendChild(square);
 }
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+  square.addEventListener("mousemove", function (e) {
+    e.target.style.background = "black";
+  });
+});
