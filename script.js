ee93c4ce5e0dd234f081b370384624398c68455e
ashxsong <beethoven6198@gmail.com>
2024-04-26 20:23:56 -0500

    Enable button to create new grid
    Modified button to generate new grid with new dimension given by user

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.querySelector("#container");
+let container = document.querySelector("#container");
 let square;
 
 for (let i = 0; i < 16 * 16; i++) {
@@ -9,7 +9,7 @@ for (let i = 0; i < 16 * 16; i++) {
   container.appendChild(square);
 }
 
-const squares = document.querySelectorAll(".square");
+let squares = document.querySelectorAll(".square");
 
 squares.forEach((square) => {
   square.addEventListener("mousemove", function (e) {
@@ -20,6 +20,29 @@ squares.forEach((square) => {
 const body = document.querySelector("body");
 const button = document.createElement("button");
 
+function newGrid(numSquares) {
+  container = document.querySelector("#container");
+  squares = document.querySelectorAll(".square");
+  let squareDim = (752 - 2 * numSquares) / numSquares;
+  let squarePixels = String(squareDim) + "px";
+  for (square of squares) {
+    container.removeChild(square);
+  }
+  for (let i = 0; i < numSquares * numSquares; i++) {
+    square = document.createElement("div");
+    square.classList.add("square");
+    square.style.height = squarePixels;
+    square.style.width = squarePixels;
+    container.appendChild(square);
+  }
+  squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("mousemove", function (e) {
+      e.target.style.background = "black";
+    });
+  });
+}
+
 button.textContent = "New Grid";
 button.style.cssText = "background-color: white; padding: 8px 18px; border-style: solid; border-width: 1px; border-color: black; color: black;"
 button.addEventListener("click", () => {
@@ -27,8 +50,7 @@ button.addEventListener("click", () => {
   while (!Number.isInteger(Number(numSquares)) || numSquares < 1 || numSquares > 100) {
     numSquares = prompt("Please enter a number between 1 and 100:");
   }
-  let squareDim = (752 - 2 * numSquares) / numSquares;
-  let squarePixels = String(squareDim) + "px";
+  newGrid(numSquares);
 });
 
 body.insertBefore(button, container);
